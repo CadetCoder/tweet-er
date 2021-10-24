37a3b30eff355579b53507c215892014e06de6d3
Darrel Toledo <darreltoledo@icloud.com>
2021-10-24 22:39:04 +0100

    form variable

--- main.js
+++ main.js
@@ -5,6 +5,8 @@ var classes => document.getElementsByClassName(classes)
 let username = id("username"),
     email = id("email"),
     password = id("password"),
+    form = id("form"),
     errorMsg = document.getElementsByClassNamee("error"),
     successIcon = classes("success-icon"),
     failureIcon = classes("failure-icon");
+
