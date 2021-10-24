4b0e5c71bdb69fcbd3bacd3867b1ae07909eca55
Darrel Toledo <darreltoledo@icloud.com>
2021-10-24 22:37:33 +0100

    target id & classes

--- main.js
+++ main.js
@@ -1,5 +1,10 @@
 var id = (id) => document.getElementById(id);
 
+var classes => document.getElementsByClassName(classes)
+
 let username = id("username"),
     email = id("email"),
-    password = id("password")
+    password = id("password"),
+    errorMsg = document.getElementsByClassNamee("error"),
+    successIcon = classes("success-icon"),
+    failureIcon = classes("failure-icon");
