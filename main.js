395ec12c2513087b0cd97d074fb032e726abfd40
Darrel Toledo <darreltoledo@icloud.com>
2021-10-25 22:42:21 +0100

    username conditional added

--- main.js
+++ main.js
@@ -10,10 +10,14 @@ let username = id("username"),
     successIcon = classes("success-icon"),
     failureIcon = classes("failure-icon");
 
-    form.addEventListener("submit", (e)=>{
+    form.addEventListener("submit", (e) => {
       e.preventDefault();
 
-      if(username.value === ''){
-        errorMsg[0].innerHTML = 'Username cannot be blank'
+      if (username.value === ""){
+        errorMsg[0].innerHTML = "Username cannot be blank";
+        failureIcon[0].style.opacity = "1";
+      } else {
+        errorMsg[0].innerHTML = "";
+        successIcon[0].style.opacity = "1";
       }
     })
